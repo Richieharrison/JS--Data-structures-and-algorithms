16bc585c80cb08822a16c8eea238c91cd4f67665
RichieHarrison <ngatiarich@gmail.com>
2024-06-11 21:29:18 +0300

    String Acces

---
+++ 4. Strings/1. StringAccess.js
@@ -0,0 +1,8 @@
+const charOne = 'dog'.charAt(1);
+// console.log(charOne);//prints o
+
+const substring = 'youtube'.substring(3, 7);
+// console.log(substring);//tube
+
+const substringOne = 'youtube'.substring(1);
+// console.log(substringOne);//outube
