2c4da48872b1e549da0f65d2d4ff3954c4fbe28d
RichieHarrison <ngatiarich@gmail.com>
2024-06-11 21:57:41 +0300

    String Replace

---
+++ 4. Strings/5. StringReplace.js
@@ -0,0 +1,5 @@
+// This replaces a specified string within a string variable with another string
+//replace(string.replaceString);
+
+const replace = "Wizard of Oz".replace("Wizard", "Witch");
+console.log(replace);
