857ec8c929880ac6e4cd6d824ffe7774a30f9c92
RichieHarrison <ngatiarich@gmail.com>
2024-06-11 21:54:27 +0300

    String Decomposition with split

--- 4. Strings/3. StringSearch.js
+++ 4. Strings/3. StringSearch.js
@@ -1,11 +1,11 @@
 // .indexOf(searchValue[, fromIndex]) -> returns position of matching string else returns -1 and is case sensitive
 
 const indexOf = 'Red Dragon'.indexOf('ed');
-//console.log(indexOf); // 1
+console.log(indexOf); // 1
 
 const startsWith = 'Red Dragon'.startsWith('ed');
-// console.log(startsWith);//false
+console.log(startsWith);//false
 
 const endsWith = 'Red Dragon'.endsWith("on");
-// console.log(endsWith);// true
+console.log(endsWith);// true
 
---
+++ 4. Strings/4. StringDecomposition.js
@@ -0,0 +1,8 @@
+// .split(separator) -> creates an array of substrings
+
+var availableFood = 'chicken, noodle, soup, broth';
+// console.log(availableFood.split(',')); // [ 'chicken', ' noodle', ' soup', ' broth' ]
+
+var test = 'chicken';
+console.log(test.split(''))
+
