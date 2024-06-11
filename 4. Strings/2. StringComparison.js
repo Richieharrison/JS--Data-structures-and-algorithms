0c076241b960a9aed078fd03ba76c0ada37c0769
RichieHarrison <ngatiarich@gmail.com>
2024-06-11 21:39:20 +0300

    String Comparison

---
+++ 4. Strings/2. StringComparison.js
@@ -0,0 +1,17 @@
+var a = 'a';
+var b = 'b';
+
+// console.log(a>b); //false
+// console.log(a<b); //true
+
+var a = 'add';
+var b = 'b';
+
+// console.log(a<b); //true
+
+var a = 'add';
+var b = 'ab';
+
+//console.log(a<b);//false;
+
+//Question: How does Js do this?
